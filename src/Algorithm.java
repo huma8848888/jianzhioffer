c1d31a8e04c5f52f27afebb54c7ec1adcc71a329
huma8848888 <329887179@qq.com>
2020-05-16 20:47:31 +0800

    Num24 链表翻转，待完善

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -10,9 +10,32 @@ public class Algorithm {
         connectNodes(node2, node3);
         connectNodes(node3, node4);
         connectNodes(node4, node5);
-        connectNodes(node5, node3);
-        System.out.printf(isCircle(node1) + "");
+        connectNodes(node5, null);
+//        System.out.printf(isCircle(node1) + "");
+        Node newNodeHead = reverse(node1);
+        printNodes(newNodeHead);
     }
+
+    //将现有链表头输入，输出新的链表头
+    public static Node reverse(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        Node temp = head.next;
+        Node newNode = reverse(head.next);
+        temp.next = head;
+        head.next = null;
+        return newNode;
+    }
+
+    public static void printNodes(Node head) {
+        Node temp = head;
+        while (temp.next != null) {
+            System.out.printf(temp.data + "");
+            temp = temp.next;
+        }
+    }
+
 
     public static void connectNodes(Node pre, Node next) {
         pre.next = next;
