04c20ae604cd8d0b0dfcbf85dd7ca937545d98b0
huma8848888 <329887179@qq.com>
2020-05-18 08:37:33 +0800

    Num34 待完善

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,5 +1,12 @@
 public class Algorithm {
     public static void main(String[] args) {
-        
+
     }
+
+    //判断一个遍历是不是二叉搜索树
+    private static void isBinaryFindTree(int[] nums) {
+
+    }
+
+
 }
